a5ec391c5578dc6c764207ba95b37a03d722a07c
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-12 05:14:55 -0300

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,25 @@
-// Include packages needed for this application
-//  to load environment variables from a .env file
+// Import mongoose to manage MongoDB interactions
 import mongoose from 'mongoose';
-const { connect, connection } = mongoose;
+
+// Mongo URI connection string 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/adviseNextStep";
 
 // connect to the database
-const connectionString = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/adviseNextStep";
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+    }
+}
 
-connect(connectionString);
+// Initialize the connection
+connectToDatabase();
 
 // Use to debug log mongo queries being executed
 mongoose.set('debug', true);
 
 // configured mongoose instance exported
 // making it available for use in app
-export default connection;
+export default mongoose.connection;
