aaf2507b2473c899602eb864afe654f4e23dae87
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-12 05:15:13 -0300

    Update Advice.js

--- server/models/Advice.js
+++ server/models/Advice.js
@@ -1,61 +1,66 @@
 import mongoose from 'mongoose';
 
-const adviceSchema = new mongoose.Schema({
-    // title: A simple string that's required for each 
-    // advice entry, to briefly describe it.
-    title: {
-        type: String,
-        required: [true, "Title is required"],
-        trim: true
+const adviceSchema = new mongoose.Schema(
+    {
+        // title: A simple string that's required for each 
+        // advice entry, to briefly describe it.
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            trim: true
+        },
+        // description: Detailed description about the advice,
+        // explaining what it is and how it can be applied.
+        description: {
+            type: String,
+            required: [true, "Description is required"],
+            trim: true
+        },
+        // createdBy: A reference to the User model. This assumes you have a User
+        //  model where each advice can be linked to a user who created it. 
+        // This helps in maintaining user-related data integrity and 
+        // facilitates user management features.
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, "Creator's User ID is required"]
+        },
+        // createdAt and updatedAt: Automatically managed fields that record
+        // when the advice was created and last updated. 
+        // sing Mongoose's timestamps option simplifies this.
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now
+        },
+        // This could be used to classify advice into different categories, 
+        // making it easier to filter or search through.
+        category: {
+            type: String,
+            required: [true, "Category is required"],
+            trim: true
+        },
+        // An array of strings, allowing multiple tags to be associated
+        // with an advice entry for better organization and searchability.
+        tags: [{
+            type: String
+        }],
+        // status: An enumeration that represents the state of the advice
+        //  (active, inactive, deprecated), useful for managing lifecycle status.
+        status: {
+            type: String,
+            enum: ['active', 'inactive', 'deprecated'],
+            default: 'active'
+        }
     },
-    // description: Detailed description about the advice,
-    // explaining what it is and how it can be applied.
-    description: {
-        type: String,
-        required: [true, "Description is required"],
-        trim: true
-    },
-    // createdBy: A reference to the User model. This assumes you have a User
-    //  model where each advice can be linked to a user who created it. 
-    // This helps in maintaining user-related data integrity and 
-    // facilitates user management features.
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, "Creator's User ID is required"]
-    },
-    // createdAt and updatedAt: Automatically managed fields that record
-    // when the advice was created and last updated. 
-    // sing Mongoose's timestamps option simplifies this.
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    },
-    // This could be used to classify advice into different categories, 
-    // making it easier to filter or search through.
-    category: {
-        type: String,
-        required: [true, "Category is required"],
-        trim: true
-    },
-    // An array of strings, allowing multiple tags to be associated
-    // with an advice entry for better organization and searchability.
-    tags: [{
-        type: String
-    }],
-    // status: An enumeration that represents the state of the advice
-    //  (active, inactive, deprecated), useful for managing lifecycle status.
-    status: {
-        type: String,
-        enum: ['active', 'inactive', 'deprecated'],
-        default: 'active'
+    // This will automatically add `createdAt` and `updatedAt` fields
+    {
+        timestamps: true
     }
-}, { timestamps: true });  // This will automatically add `createdAt` and `updatedAt` fields
+);
 
-const Advice = mongoose.model('Advice', adviceSchema);
-
-export default Advice;
+// User exported making it available for use in app
+export default mongoose.model('Advice', adviceSchema);
