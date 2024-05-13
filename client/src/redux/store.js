5ba01e4c0eff70606260a20f0e1565b909a30c89
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-13 04:17:40 -0300

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers'; // Your root reducer
 
-const store = createStore(rootReducer);
-
-export default store;
+export default configureStore({
+    reducer: rootReducer
+});
