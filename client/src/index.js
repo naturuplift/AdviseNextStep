ad526de11edb06dca5848d606336b8f4a13ce7a9
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-13 04:17:47 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/App';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+// Use the createRoot API for React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Call reportWebVitals if needed
 reportWebVitals();
