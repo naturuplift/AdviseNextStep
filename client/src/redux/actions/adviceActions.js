4bec759e6740c2df6a3e60236699ed54fe2c4bc6
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-17 23:16:49 -0300

    Update adviceActions.js

--- client/src/redux/actions/adviceActions.js
+++ client/src/redux/actions/adviceActions.js
@@ -1,7 +1,7 @@
 
 export const fetchAdvice = (question) => async (dispatch) => {
     try {
-        const response = await fetch('/api/advice', {
+        const response = await fetch('http://localhost:3002/api/createAdviceRequest', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -17,6 +17,29 @@ export const fetchAdvice = (question) => async (dispatch) => {
         console.error('Failed to fetch advice:', error);
         dispatch({
             type: 'FETCH_ADVICE_FAILURE',
+            error: error.toString()
+        });
+    }
+};
+
+export const createAdvice = (adviceData) => async (dispatch) => {
+    try {
+        const response = await fetch('http://localhost:3002/api/advice', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(adviceData)
+        });
+        const data = await response.json();
+        dispatch({
+            type: 'CREATE_ADVICE_SUCCESS',
+            payload: data
+        });
+    } catch (error) {
+        console.error('Failed to create advice:', error);
+        dispatch({
+            type: 'CREATE_ADVICE_FAILURE',
             error: error.toString()
         });
     }
