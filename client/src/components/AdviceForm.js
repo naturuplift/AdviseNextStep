930bc73ffe5f7c6c6b4ee705c1f54fd7da9e0a19
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-19 22:05:51 -0300

    Update AdviceForm.js

--- client/src/components/AdviceForm.js
+++ client/src/components/AdviceForm.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createAdvice } from '../redux/actions/adviceActions';
 
 function AdviceForm() {
@@ -15,7 +15,7 @@ function AdviceForm() {
     });
 
     const dispatch = useDispatch();
-    // const adviceResponse = useSelector(state => state.advice.advice);
+    const adviceResponse = useSelector(state => state.advice.advice);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -23,42 +23,39 @@ function AdviceForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            {/* Example for title, add similar inputs for description, category, etc. */}
-            <input
-                type="text"
-                value={adviceData.title}
-                onChange={e => setAdviceData({...adviceData, title: e.target.value})}
-                placeholder="Enter advice title"
-                required
-            />
-            <button type="submit">Submit Advice</button>
-        </form>
+        <div className="AdviceForm">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={adviceData.title}
+                    onChange={e => setAdviceData({...adviceData, title: e.target.value})}
+                    placeholder="Enter advice title"
+                    required
+                />
+                <input
+                    type="text"
+                    value={adviceData.description}
+                    onChange={e => setAdviceData({...adviceData, description: e.target.value})}
+                    placeholder="Enter advice description"
+                    required
+                />
+                <input
+                    type="text"
+                    value={adviceData.category}
+                    onChange={e => setAdviceData({...adviceData, category: e.target.value})}
+                    placeholder="Enter advice category"
+                    required
+                />
+                <button type="submit">Submit Advice</button>
+            </form>
+            {adviceResponse && (
+                <div>
+                    <h4>Response:</h4>
+                    <pre>{JSON.stringify(adviceResponse, null, 2)}</pre>
+                </div>
+            )}
+        </div>
     );
 }
-
-// function AdviceForm() {
-//     const [question, setQuestion] = useState('');
-//     const dispatch = useDispatch();
-
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         // Assuming fetchAdvice is a thunk action creator
-//         dispatch(createAdvice(question));
-//     };
-
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <input
-//                 type="text"
-//                 value={question}
-//                 onChange={e => setQuestion(e.target.value)}
-//                 placeholder="Enter your question"
-//                 required
-//             />
-//             <button type="submit">Get Advice</button>
-//         </form>
-//     );
-// }
 
 export default AdviceForm;
