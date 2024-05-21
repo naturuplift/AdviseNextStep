232863fafafb030126aa3186a8095c6929a0339f
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-05-20 23:59:02 -0300

    Update adviceActions.js

--- client/src/redux/actions/adviceActions.js
+++ client/src/redux/actions/adviceActions.js
@@ -44,3 +44,26 @@ export const createAdvice = (adviceData) => async (dispatch) => {
         });
     }
 };
+
+export const fetchAdviceFromOpenAI = (question) => async (dispatch) => {
+    try {
+      const response = await fetch('http://localhost:3002/api/advice/openai', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question })
+      });
+      const data = await response.json();
+      dispatch({
+        type: 'FETCH_ADVICE_SUCCESS',
+        payload: data.recommendation
+      });
+    } catch (error) {
+      console.error('Failed to fetch advice:', error);
+      dispatch({
+        type: 'FETCH_ADVICE_FAILURE',
+        error: error.toString()
+      });
+    }
+  };
